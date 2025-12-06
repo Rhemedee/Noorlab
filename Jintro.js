e9bff146bb00870c9f40238e60f89c457d119599
Anifowose AbdulRahman <tobiowolabi1119@gmail.com>
2025-12-06 23:23:22 +0100

    Function

--- Jintro.js
+++ Jintro.js
@@ -137,3 +137,108 @@ if (age >= 16 && country === 'USA') {
 } else {
   console.log('You are not eligible to get a driving license.');
 }
+
+// JavaScript Functions
+// Declaring a function
+// To declare a function, you use the function keyword, followed by the function name, a list of parameters, and the function body as follows:
+
+// function functionName(parameters) {
+//     // function body
+//     // ...
+// }
+
+function greet(user){
+  console.log("Hello " + user + ", welcome to our website!");
+}
+greet("AbdulRahman");
+greet("Anifowose");
+
+// Calling a function
+// To use a function, you need to call it. Calling a function is also known as invoking a function.
+
+// To call a function, you use its name followed by arguments enclosed in parentheses like this:
+
+// functionName(arguments);
+
+function add(a, b){
+  return a + b;
+}
+let sum = add(3, 7);
+console.log(sum);
+
+// Comparing Function Values
+
+function compare(a, b){
+  if (a < b) {
+    return -1;
+  }
+
+  if (a > b) {
+    return 1
+  }
+}
+
+// The function immediately stops executing when it reaches the return statement. Therefore, you can use the return statement without a value to exit the function prematurely, like this:
+
+function say(message) {
+  // show nothing if the message is empty
+  if (!message) {
+    return;
+  }
+  console.log(message);
+}
+
+// The arguments object
+// Inside a function, you can access an object called arguments that represents the named arguments of the function.
+
+// The arguments object behaves like an array though it is not an instance of the Array type.
+
+// For example, you can use the square bracket [] to access the arguments: arguments[0] returns the first argument, arguments[1] returns the second one, and so on.
+
+// You can also use the length property of the arguments object to determine the number of arguments.
+
+// The following example implements a generic add() function that calculates the sum of any number of arguments.
+
+function add() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+// Code language: JavaScript (javascript)
+// Hence, you can pass any number of arguments to the add() function, like this:
+
+// console.log(add(1, 2)); // 3
+// console.log(add(1, 2, 3, 4, 5)); // 15
+// Code language: JavaScript (javascript)
+// Function hoisting
+// In JavaScript, you can use a function before declaring it. For example:
+
+showMe(); // a hoisting example
+
+function showMe() {
+  console.log('an hoisting example');
+}
+// Code language: JavaScript (javascript)
+// This feature is called hoisting.
+
+// Function hoisting is a mechanism in which the JavaScript engine physically moves function declarations to the top of the code before executing them.
+
+// The following shows the version of the code before the JavaScript engine executes it:
+
+function showMe() {
+  console.log('a hoisting example');
+}
+
+showMe(); // a hoisting example
+// Code language: JavaScript (javascript)
+// Function hoisting allows you to call a function before declaring it, making the development workflow more smoothly.
+
+// Summary
+// Use the function keyword to declare a function.
+// Use the functionName() to call a function.
+// All functions implicitly return undefined if they don’t explicitly return a value.
+// Use the return statement to return a value from a function explicitly.
+// The arguments variable is an array-like object inside a function, representing function arguments.
+// The function hoisting allows you to call a function before declaring it.
